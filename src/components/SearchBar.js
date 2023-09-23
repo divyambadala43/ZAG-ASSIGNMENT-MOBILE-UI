15b0c0587c8a2349285a52e2177b3462a9617580
divyambadala43 <divyambadala43@gmail.com>
2023-09-24 04:33:35 +0530

    FIX: SVG JSX Fixes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,9 +14,9 @@ const SearchBar = () => {
           <path
             d="M21 21L17.5001 17.5M20 11.5C20 16.1944 16.1944 20 11.5 20C6.80558 20 3 16.1944 3 11.5C3 6.80558 6.80558 3 11.5 3C16.1944 3 20 6.80558 20 11.5Z"
             stroke="black"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
         <div className={styles.placeholder}>Search anything</div>
@@ -31,9 +31,9 @@ const SearchBar = () => {
           <path
             d="M6 12H18M3 6H21M9 18H15"
             stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       </div>
